02ecf78d3d9be9911a25e7e1019f951331b22afb
christopheryoder <christophercyoder@gmail.com>
2014-09-02 17:57:17 -0400

    Version for DanA

--- js/app.js
+++ js/app.js
@@ -1,14 +1,45 @@
+/*Wait for the document to load before running*/
+
 $(document).ready(function() {
-	$( "li.item" ).hover(
-		function() {
-		$(this).append( "<button>X</button>" );
-		$( ":button" ).click(function() {
-		$(this).closest("li").remove();
-		/*delete this whole list item*/
+	/*When the user presses the 'enter' key while in the text input box*/
+
+	$('input:text').keyup(function(event){
+		if(event.keyCode == 13) {
+
+			/*get user input*/
+
+			var userinput = $(this).val();
+
+			/*Add a list item with a checkbox with the user input as a label immediately before this text box' list item*/
+
+			$(this).closest("li").before("<li class='item'><input type='checkbox'><label>Butter</label></li>");
+		};
 	});
-	}, 
-		function() {
-		$(this).find( "button" ).remove();
-	}
-	);
+
+	/*When mouse enters an li with the item class:*/
+
+	$( "li.item" ).mouseenter(function() {
+
+		/*add an X button after that item*/
+
+		$(this).append("<button>X</button>");
+
+		/*when user clicks on this button*/
+
+		$( ":button" ).click(function() {
+
+		/*delete this whole list item*/
+
+		$(this).closest("li").remove();
+		});
+	});
+
+	/*when mouse leaves an li with the item class:*/
+
+	$( "li.item" ).mouseleave(function() {
+
+			/*remove the X button*/
+
+			$(this).find( "button" ).remove();
+	});
 });
