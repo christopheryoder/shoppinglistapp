b317d9e032af0ab687f45bb2ea9b3727f32711b2
christopheryoder <christophercyoder@gmail.com>
2014-09-03 10:50:29 -0400

    Finished Shopping List

--- js/app.js
+++ js/app.js
@@ -1,45 +1,52 @@
 /*Wait for the document to load before running*/
-
 $(document).ready(function() {
-	/*When the user presses the 'enter' key while in the text input box*/
 
-	$('input:text').keyup(function(event){
-		if(event.keyCode == 13) {
+    /*When the user releases the 'enter' key while in the text input box*/
+    $('input:text').keyup(function(event){
+            if(event.keyCode == 13) {
 
-			/*get user input*/
+                /*get user input*/
+                var userinput = $(this).val();
 
-			var userinput = $(this).val();
+                /*Add a list item with a checkbox with the user input as a label immediately before this text box' list item*/
+                $(this).closest("li").before("<li class='item'><input type='checkbox'><label>" + userinput + "</label></li>");
 
-			/*Add a list item with a checkbox with the user input as a label immediately before this text box' list item*/
+                /*delete contents of text box after user hits enter*/
+                $(this).val('');
+            };
+    });
 
-			$(this).closest("li").before("<li class='item'><input type='checkbox'><label>Butter</label></li>");
-		};
-	});
+    /*When the user clicks the add button while in the text input box*/
+    $('#add').click(function() {
 
-	/*When mouse enters an li with the item class:*/
+        /*get user input*/
+        var userinput = $(this).siblings('input:text').val();
 
-	$( "li.item" ).mouseenter(function() {
+        /*Add a list item with a checkbox with the user input as a label immediately before this text box' list item*/
+        $(this).closest("li").before("<li class='item'><input type='checkbox'><label>" + userinput + "</label></li>");
 
-		/*add an X button after that item*/
+        /*delete contents of text box after user clicks 'add'*/
+        $(this).siblings('input:text').val('');
+    });
+});
 
-		$(this).append("<button>X</button>");
+/*When mouse enters an li with the item class:*/
+$(document).on("mouseenter", "li.item", function(){
 
-		/*when user clicks on this button*/
+	/*add an X button after that item*/
+    $(this).append("<button id='x'>X</button>");
+ 
+	/*When the user clicks on an X button...*/
+	$( "#x" ).click(function() {
 
-		$( ":button" ).click(function() {
-
-		/*delete this whole list item*/
-
-		$(this).closest("li").remove();
-		});
-	});
-
+	/*Remove this list item*/
+	$(this).closest("li").remove();
+        });
+});
+ 
 	/*when mouse leaves an li with the item class:*/
+$(document).on("mouseleave", "li.item", function(){
 
-	$( "li.item" ).mouseleave(function() {
-
-			/*remove the X button*/
-
-			$(this).find( "button" ).remove();
-	});
+    /*remove the X button*/
+    $(this).find( "button" ).remove();
 });
